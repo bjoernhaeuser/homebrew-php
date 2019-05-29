eac9df962bfefad15bbdfa3c3fe663b03ba140ac
Björn Häuser <b.haeuser@rebuy.com>
2019-05-29 10:21:49 +0200

    Add php73 grpc

---
+++ Formula/php73-grpc.rb
@@ -0,0 +1,20 @@
+require File.expand_path("../Abstract/abstract-php-extension", __dir__)
+
+class Php73Grpc < AbstractPhp73Extension
+  init
+  desc "The PHP extension for the gRPC library"
+  homepage "https://pecl.php.net/package/grpc"
+  url "https://pecl.php.net/get/grpc-1.20.0.tgz"
+  sha256 "1dacfeb1e744f86c6217cb84f1f8b31dd38fbb644e1cba8ebb3590f0d8417485"
+
+  def install
+    Dir.chdir "grpc-#{version}"
+    safe_phpize
+    system "./configure", "--enable-grpc=#{HOMEBREW_PREFIX}",
+           "--prefix=#{prefix}", phpconfig,
+           "CFLAGS=-Ithird_party/boringssl/include"
+    system "make"
+    prefix.install "modules/grpc.so"
+    write_config_file if build.with? "config-file"
+  end
+end
