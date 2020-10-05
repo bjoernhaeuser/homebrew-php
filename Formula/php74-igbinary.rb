948b4dcd583b7f42d4a54c926f920e9f4f66584d
Björn Häuser <b.haeuser@rebuy.com>
2020-10-05 21:19:41 +0200

    Correct igbinary

--- Formula/php74-igbinary.rb
+++ Formula/php74-igbinary.rb
@@ -4,7 +4,7 @@ class Php74Igbinary < AbstractPhp74Extension
   init
   desc "Igbinary is a replacement for the standard php serializer."
   homepage "https://pecl.php.net/package/igbinary"
-  url "https://github.com/igbinary/igbinary/archive/3.1.5.tar.gz"
+  url "https://pecl.php.net/get/igbinary-3.1.5.tgz"
   sha256 "e1f7dc74233052eb64d251c00e95889110de2406cdf5e71139050194da596d92"
   head "https://github.com/igbinary/igbinary.git"
 
